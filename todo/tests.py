d69e80d0c04f111dcb4a2d81a39273eef1d64409
1F10240122 <s1F102401225@iniad,org>
2025-07-03 14:10:54 +0900

    Flake8の警告を修正

--- todo/tests.py
+++ todo/tests.py
@@ -3,10 +3,12 @@ from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
+
 # Create your tests here.
 class SampleTestCase(TestCase):
     def test_sample1(self):
         self.assertEqual(1 + 2, 3)
+
 
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
@@ -22,7 +24,6 @@ class TaskModelTestCase(TestCase):
     def test_create_task2(self):
         task =Task(title='task2')
         task.save()
-
         task = Task.objects.get(pk=task.pk)
         self.assertEqual(task.title, 'task2')
         self.assertFalse(task.completed)
@@ -52,6 +53,7 @@ class TaskModelTestCase(TestCase):
 
         self.assertFalse(task.is_overdue(current))
 
+
 class TodoViewsTestCase(TestCase):
     def test_index_get(self):
         client = Client()
@@ -69,7 +71,7 @@ class TodoViewsTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(len(response.context['tasks']), 1)
-        
+
     def test_index_get_order_post(self):
         task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task1.save()
